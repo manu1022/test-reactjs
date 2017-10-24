5b31862c929ff2ff5c0912b3efc5fffb09249f95
manuel <manuel@clarive.com>
2017-10-24 12:16:37 +0200

    comment tests

--- test/Comment.spec.js
+++ test/Comment.spec.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Comment from '../src/components/Comment';
+import CommentList from '../src/components/CommentsList';
+import { spy } from 'sinon';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { mount, render, shallow } from 'enzyme';
 
 describe('Comment item', () => {
   const wrapper = shallow(<Comment />);
@@ -7,4 +12,21 @@ describe('Comment item', () => {
   it('should be a list item', () => {
     expect(wrapper.type()).to.eql('li');
   });
+  
+  it('renders the custom comment text', () => {
+  wrapper.setProps({ comment: 'sympathetic ink' });
+  expect(wrapper.contains('sympathetic ink')).to.equal(true);
+  });
+  
+  it('has a class name of "comment-item"', () => {
+    expect(wrapper.find('li.comment-item')).to.have.length(1);
+  });
+
+  it('calls componentDidMount', () => {
+	spy(CommentList.prototype, 'componentDidMount');
+
+    const wrapper = mount(<CommentList />);
+    expect(CommentList.prototype.componentDidMount.calledOnce).to.equal(true);
+  });
+
 });
